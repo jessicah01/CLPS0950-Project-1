114b114f638a36660d9f1822766db2781590c0ea
Isaac Kim <snakeisaac@gmail.com>
2022-03-17 06:18:29 -0400

    Greatly increase efficiency of board validation ismember takes a lot of computational time, so I removed all instances of it from the validation function and replaced it with alternatives.

--- checkBoardState.m
+++ checkBoardState.m
@@ -14,6 +14,8 @@ function state = validation(boardState)
 %define state initially as empty string
 state = "";
 
+[rows, cols] = size(boardState);
+
 %player 1 and 2 horizontal win matrix, also used to check for diags
 p1winH = [1,1,1,1];
 p2winH = [2,2,2,2];
@@ -24,22 +26,40 @@ p2winV = [2;2;2;2];
 
 %checks if board is full yet validation function ran implying no winner
 %thus draw
-if ~ismember(0,boardState)
+if all(boardState(:) ~= 0)
   state = GameState.DRAW;
   return;
 end
 
 %checks for horizontal win scenario, returns if win condition reached
-for j = 1:4
-  %sets checkBoardState as area of 6*4 rectangle
-  checkBoardState = boardState(1:6,0+j:3+j);
-  if all(ismember(p1winH,checkBoardState, 'rows'))
-      state = GameState.PLAYER_1_WIN;
-      return;
-  elseif all(ismember(p2winH,checkBoardState, 'rows'))
-      state = GameState.PLAYER_2_WIN;
-      return;
-  end
+% Isaac - rewriting this to increase the speed of execution. MCTS takes
+% forever to execute due to board state validation taking too long
+% Main function taking time, according to profiler, is ismember - so I
+% reduced dependence on that function as much as possible
+for row = 1:rows
+    count = 1;
+    prev = 0;
+    for col = 1:cols
+        if boardState(row, col) == prev && boardState(row, col) ~= 0
+            count = count + 1;
+        else
+            count = 1;
+            prev = boardState(row, col);
+        end
+        if count >= 4
+            if prev == 1
+                state = GameState.PLAYER_1_WIN;
+            elseif prev == 2
+                state = GameState.PLAYER_2_WIN;
+            else
+                disp('Error - Invalid State')
+            end
+            return;
+        end
+    end
+    if state ~= ""
+        return;
+    end
 end
 
 %checks for vertical win scenario, breaks loop if win condition reached
@@ -47,10 +67,10 @@ for i = 1:3
   for j = 1:7
       %sets checkBoardState as 1*4 column vector
       checkBoardState = boardState(0+i:3+i,j);
-      if all(p1winV == checkBoardState)
+      if isequal(p1winV, checkBoardState)
           state = GameState.PLAYER_1_WIN;
           return;
-      elseif all(p2winV == checkBoardState)
+      elseif isequal(p2winV, checkBoardState)
           state = GameState.PLAYER_2_WIN;
           return;
       end
@@ -63,19 +83,13 @@ for i = 1:3
   for j = 1:4
       %bottom to top diagonal (/)
       A = diag(boardState(0+i:3+i,0+j:3+j));
-      %reshape from column to row vector
-      check = reshape(A,1,4);
       %top to bottom diagonal (\)
       B = diag(flip(boardState(0+i:3+i,0+j:3+j)));
-      %reshape from column to row vector
-      flipCheck = reshape(B,1,4);
-      %concatenate two 1*4 matricies into a single 2*4 matrix
-      checkBoardState = [check;flipCheck];
 
-      if ismember(p1winH,checkBoardState, 'rows')
+      if isequal(A, p1winV) || isequal(B, p1winV)
           state = GameState.PLAYER_1_WIN;
           return;
-      elseif ismember(p2winH,checkBoardState, 'rows')
+      elseif isequal(A, p2winV) || isequal(B, p2winV)
           state = GameState.PLAYER_2_WIN;
           return;
       end
